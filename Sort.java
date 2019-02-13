0d7db9bad7ce8b9bcf89f86d3f5d761d8142014f
Justin <37247743+globalkitteh@users.noreply.github.com>
2019-02-13 08:54:35 -0600

    Bubble Sort
    Sorter works for Bublé sort, going to make it BOGO

--- Sort.java
+++ Sort.java
@@ -1,12 +1,28 @@
 import java.util.Arrays;
 
-class Main 
-{ 
+class Sort { 
 
     // start and end are optional - some sorting algorithms use them, but you can ignore them
     void sort(double arr[], long start, long end) 
     { 
-      // sort the array here
+      for(int x=0; x <= (arr.length-1); x++)
+        System.out.println(arr[x]);
+        
+        double tempNum;
+        double tempNum2;
+        for (int numLoop = 0; numLoop < (((end * end) + 1) / 2); numLoop++){
+          for (int i = 0; i < (end-1); i++) {
+            
+            tempNum = arr[i];
+            tempNum2 = arr[i+1];
+            
+            if(tempNum > tempNum2){
+              
+              arr[i+1] = tempNum;
+              arr[i] = tempNum2;
+            }
+          }
+        }
     } 
 
   
@@ -18,13 +34,13 @@ class Main
         System.out.println(Arrays.toString(arr));
         
         // create a sorted version to test.
-        double sortedArr[] = arr;
+        double sortedArr[] = arr.clone();
         Arrays.sort(sortedArr);
         System.out.println("sorted array: "); 
         System.out.println(Arrays.toString(sortedArr));
         
        
-        Main sorter = new Main(); // create a sorter object
+        Sort sorter = new Sort(); // create a sorter object
         sorter.sort(arr, 0, arr.length); // call the sort function
   
         System.out.println("*** your sorted array: "); 
