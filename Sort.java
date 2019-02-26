9e4f381122304eef8ee34243642e3825ee033b4e
globalkitteh <37247743+globalkitteh@users.noreply.github.com>
2019-02-26 10:22:01 -0600

    BOGO "functional"
    Made BOGO its own method, changed while to a double =, changed main method to work with new methods

--- Sort.java
+++ Sort.java
@@ -1,86 +1,90 @@
 import java.util.Arrays;
 import java.util.Random;
 
-class Main { 
-
-    // start and end are optional - some sorting algorithms use them, but you can ignore them
-    void sort(double arr[], long start, long end) 
-    {
-      double initArr[] = arr;
-      //System.out.println(Arrays.toString(initArr)); //All of these output the initial array, not the sorted one
-      //System.out.println("input array");
-      //System.out.println(Arrays.toString(arr));
-      for(int x=0; x <= (arr.length-1); x++){
-        System.out.println(arr[x]);
-        
-        double tempNum;
-        double tempNum2;
-        for (int numLoop = 0; numLoop < (((end * end) + 1) / 2); numLoop++){
-          for (int i = 0; i < (end-1); i++) {
+class Sort { 
+  
+  double correctSort[];
+  
+  // start and end are optional - some sorting algorithms use them, but you can ignore them
+  void sort(double arr[], long start, long end) 
+  {
+    for(int x=0; x <= (arr.length-1); x++){
+      System.out.println(arr[x]);
+      
+      double tempNum;
+      double tempNum2;
+      for (int numLoop = 0; numLoop < (((end * end) + 1) / 2); numLoop++){
+        for (int i = 0; i < (end-1); i++) {
+          
+          tempNum = arr[i];
+          tempNum2 = arr[i+1];
+          
+          if(tempNum > tempNum2){
             
-            tempNum = arr[i];
-            tempNum2 = arr[i+1];
-            
-            if(tempNum > tempNum2){
-              
-              arr[i+1] = tempNum;
-              arr[i] = tempNum2;
-            }
+            arr[i+1] = tempNum;
+            arr[i] = tempNum2;
           }
         }
       }
-      
-      //GO BOGO
-      
-      boolean doneBOGO = false;
-      Random rand = new Random();
-      double arrBOGO[] = initArr;
-      double tempNum;
-      double tempNum2;
-      
-      System.out.println("BOGO Array");
-      System.out.println(Arrays.toString(arrBOGO));
-      System.out.println("Initial Array");
-      System.out.println(Arrays.toString(initArr));
-      
-      
-      while (doneBOGO = false){ //loop bogo until it succeeds
-        for (int i = 0; i < (end-1); i++) {
-          tempNum = arrBOGO[i];
-          tempNum2 = arrBOGO[i+1]; //sets temps for switching purposes
-          
-          int randOutput = rand.nextInt(10); //creates random int between 0 and 10
-          
-          if(randOutput >= 5){
-            arrBOGO[i+1] = tempNum; //if random is above or equal to 5, switch the two numbers
-            arrBOGO[i] = tempNum2;
-            }
-        }
-        System.out.println(arrBOGO);
-        if(arrBOGO == arr) {
-          doneBOGO = true;
+    }
+    correctSort = arr;
+  }
+  
+  void sortBOGO(double arrBOGO[], long start, long end)
+  {
+    
+    //GO BOGO
+    double initArr[] = arrBOGO;
+    boolean doneBOGO = false;
+    Random rand = new Random();
+    double tempNum;
+    double tempNum2;
+    
+    System.out.println("Initial BOGO");
+    System.out.println(Arrays.toString(arrBOGO));      
+    System.out.println("Correct sort");
+    System.out.println(Arrays.toString(correctSort));   
+    
+    while(doneBOGO == false){ //loop bogo until it succeeds
+      for (int i = 0; i < (end-1); i++) {
+        tempNum = arrBOGO[i];
+        tempNum2 = arrBOGO[i+1]; //sets temps for switching purposes
+        
+        int randOutput = rand.nextInt(10); //creates random int between 0 and 10
+        
+        if(randOutput >= 5){
+          arrBOGO[i+1] = tempNum; //if random is above or equal to 5, switch the two numbers
+          arrBOGO[i] = tempNum2;
         }
       }
+      //System.out.println(Arrays.toString(arrBOGO)); // commented out because causes lag, slows down BOGOing
+      if(arrBOGO == correctSort) { //if the generated array matches the correct sort, it ends
+        doneBOGO = true;
+        System.out.println("BOGO SUCCESSFUL");
+      }
     }
+  }
   
-    // Test program 
-    public static void main(String args[]) 
-    { 
-        double arr[] = {10.0, 7.1, 8.6, 9.9, 1.8, 5.0}; 
-        System.out.println("original array: "); 
-        System.out.println(Arrays.toString(arr));
-        
-        // create a sorted version to test.
-        double sortedArr[] = arr.clone();
-        Arrays.sort(sortedArr);
-        System.out.println("sorted array: "); 
-        System.out.println(Arrays.toString(sortedArr));
-        
-       
-        Sort sorter = new Sort(); // create a sorter object
-        sorter.sort(arr, 0, arr.length); // call the sort function
-  
-        System.out.println("*** your sorted array: "); 
-        System.out.println(Arrays.toString(arr));
-    } 
+  // Test program 
+  public static void main(String args[]) 
+  { 
+    double arr[] = {10.0, 7.1, 8.6, 9.9, 1.8, 5.0}; 
+    System.out.println("original array: "); 
+    System.out.println(Arrays.toString(arr));
+    
+    // create a sorted version to test.
+    double sortedArr[] = arr.clone();
+    Arrays.sort(sortedArr);
+    System.out.println("sorted array: "); 
+    System.out.println(Arrays.toString(sortedArr));
+    
+    double bubbleArr[] = arr.clone();
+    
+    Sort sorter = new Sort(); // create a sorter object
+    sorter.sort(bubbleArr, 0, bubbleArr.length); // call the sort function
+    
+    sorter.sortBOGO(arr, 0, arr.length); // call the BOGOsort function
+    System.out.println("*** your sorted array: "); 
+    System.out.println(Arrays.toString(arr));
+  } 
 } 
